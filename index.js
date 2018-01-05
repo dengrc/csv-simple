018351d32d3c672bc407cfeb790968176b9a0176
dengrc <1107151332@qq.com>
2018-01-05 14:39:13 +0800

    字段换行符处理
    字段换行符处理，优化读取

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-﻿const readline = require('readline');
-const fs = require('fs');
+﻿const fs = require('fs');
 const os = require('os');
 
+const EOL = os.EOL;
 const CSVsimple = module.exports;
 const regSeparator = /,/;
 const regA = /"/;
 const regAg = /"/g;
 const regAA = /""/;
 const regAAg = /""/g;
+const regEOL = new RegExp("\\r\\n");
+const regEOLg = new RegExp("\\r\\n", 'g');
 
 function stringify(value) {
 	if(!value) {
 		return value === 0 || value === false ? value : ""
 	}
 	if(typeof value === "string") {
-		if(regA.test(value)) {
+		if(regEOL.test(value)) {
+			return '"' + value.replace(regEOLg, '\n').replace(regAg, '""') + '"'
+		} else if(regA.test(value)) {
 			return '"' + value.replace(regAg, '""') + '"'
 		} else if(regSeparator.test(value)) {
 			return '"' + value + '"'
@@ -26,9 +30,9 @@ function stringify(value) {
 
 function parseArray(value) {
 	if(regA.test(value)) {
-		var isA = true,
-			cacheString,
-			array = [];
+		let isA = true,
+			cacheString;
+		const array = [];
 		value.split(",").forEach(function(v, i) {
 			if(isA) {
 				if(regA.test(v)) {
@@ -45,8 +49,8 @@ function parseArray(value) {
 				if(regA.test(v)) {
 					if(v.match(regAg).length % 2) {
 						isA = true;
-						cacheString.push(v.substring(0, v.length - 1).replace(regAAg, '"'))
-						array.push(cacheString);
+						cacheString.push(v.substring(0, v.length - 1).replace(regAAg, '"'));
+						array.push(cacheString.join(","))
 					} else {
 						cacheString.push(v.replace(regAAg, '"'))
 					}
@@ -55,7 +59,7 @@ function parseArray(value) {
 				}
 			}
 		});
-		return array;
+		return array
 	}
 	return value.split(",")
 }
@@ -64,7 +68,6 @@ CSVsimple.write = (path, array) => {
 	return new Promise((resolve, reject) => {
 		const keys = Object.keys(array[0] || {}),
 			fWrite = fs.createWriteStream(path),
-			lineBreaks = os.EOL,
 			CATCH_Lines = 1000,
 			MAX_Index = array.length - 1;
 
@@ -76,56 +79,50 @@ CSVsimple.write = (path, array) => {
 			cache.push(keys.join(","));
 			array.forEach((item, i) => {
 				let row = [];
-				keys.forEach(key => {
-					row.push(stringify(item[key]))
-				});
+				keys.forEach(key => row.push(stringify(item[key])));
 				index++;
 				cache.push(row.join(","))
 				if(index === CATCH_Lines || i === MAX_Index) {
-					fWrite.write(cache.join(lineBreaks));
-					cache.length = index = 0;
+					fWrite.write(cache.join(EOL));
+					cache.length = index = 0
 				}
 			});
-			fWrite.end();
+			fWrite.end()
 		});
 
-		fWrite.on('error', (err) => {
-			reject(err);
-		});
+		fWrite.on('error', (err) => reject(err));
 
-		fWrite.on('finish', () => {
-			resolve(true);
-		});
+		fWrite.on('finish', () => resolve(true));
 	})
 }
 
 CSVsimple.read = path => {
 	return new Promise((resolve, reject) => {
-		const fRead = fs.createReadStream(path),
-			lineBreaks = os.EOL,
-			nLength = lineBreaks.length;
-			
+		const fRead = fs.createReadStream(path);
+
 		let fields = [],
 			i = 0,
 			_buffer = new Buffer(0),
 			result = [];
 
 		function read(buffer) {
+			let index = -1,
+				offset = 0;
 			do {
-				var n = buffer.indexOf(lineBreaks);
-				if(n > -1) {
-					line(buffer.slice(0, n).toString())
-					buffer = buffer.slice(n + nLength)
+				index = buffer.indexOf(EOL, offset);
+				if(index > -1) {
+					line(buffer.slice(offset, index).toString())
+					offset = index + EOL.length
 				}
-			} while (n > -1)
-			return buffer;
+			} while (index > -1)
+			return offset ? buffer.slice(offset) : buffer
 		}
 
 		function line(value) {
 			if(i++) {
 				let row = {},
 					array = parseArray(value);
-				fields.forEach(function(field, i) {
+				fields.forEach((field, i) => {
 					row[field] = array[i]
 				});
 				result.push(row)
@@ -137,16 +134,14 @@ CSVsimple.read = path => {
 		fRead.on('data', (chunk) => {
 			fRead.pause();
 			_buffer = read(Buffer.concat([_buffer, chunk]));
-			fRead.resume();
+			fRead.resume()
 		});
-		
+
 		fRead.on('end', () => {
 			_buffer.length && line(_buffer.toString());
-			resolve(result);
+			resolve(result)
 		});
-		
-		fRead.on('error', (err) => {
-			reject(e)
-		});
+
+		fRead.on('error', (err) => reject(e));
 	})
 }
--- test/write.js
+++ test/write.js
@@ -1,14 +1,14 @@
 const csv = require('../index.js');
 const os = require('os');
-csv.write("./file.csv",Array.from(new Array(3),function(a,i){
-	return i%2?{
-        a: 1,
+csv.write("./file.csv", Array.from(new Array(30), function(a, i) {
+	return i % 2 ? {
+		a: 1,
 		b: true,
-		c: '看到发卡的看\n法地方'+os.EOL
-	}:{
+		c: '看到发卡的看\n法地方' + os.EOL + "aaa"
+	} : {
 		a: 2,
 		b: false,
-		c: "ddddpoo破,i哦ii！@#￥……&****（（"
+		c: "aaa" + os.EOL + "ddddpoo破,i哦ii！@#￥……&****（（"
 	}
 })).then(() => {
 	console.log("success");
